d5b0c7004d63b305cf64968ce2d553a3e0558da6
johc <johc@itu.dk>
2024-03-21 12:46:01 +0100

    Hash routing strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataComponent } from './pages/data/data.component';
 import { StatsComponent } from './pages/stats/stats.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const routes: Routes = [
-  { path: 'data', component: DataComponent},
-  { path: 'stats', component: StatsComponent},
+  { path: 'data', component: DataComponent },
+  { path: 'stats', component: StatsComponent },
   { path: '', redirectTo: '/stats', pathMatch: 'full' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
 })
 export class AppRoutingModule { }
