f3e8a2c84d2fafd99e0befdcd93d86765b58c8a3
johc <johc@itu.dk>
2024-03-21 12:55:28 +0100

    Revert hash routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataComponent } from './pages/data/data.component';
 import { StatsComponent } from './pages/stats/stats.component';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const routes: Routes = [
-  { path: 'data', component: DataComponent },
-  { path: 'stats', component: StatsComponent },
+  { path: 'data', component: DataComponent},
+  { path: 'stats', component: StatsComponent},
   { path: '', redirectTo: '/stats', pathMatch: 'full' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
