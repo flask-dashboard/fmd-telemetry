097592f9a898e76e7379662b486becf7801c2d79
johc <johc@itu.dk>
2024-03-21 15:51:38 +0100

    Folder moving

---
+++ .angular/cache/17.1.3/vite/deps/browser-6SEB2AEU.js
@@ -0,0 +1,52 @@
+import {
+  Animation,
+  AnimationDriver,
+  AnimationEngine,
+  AnimationRenderer,
+  AnimationRendererFactory,
+  AnimationStyleNormalizer,
+  BaseAnimationRenderer,
+  NoopAnimationDriver,
+  NoopAnimationStyleNormalizer,
+  WebAnimationsDriver,
+  WebAnimationsPlayer,
+  WebAnimationsStyleNormalizer,
+  allowPreviousPlayerStylesMerge,
+  camelCaseToDashCase,
+  containsElement,
+  createEngine,
+  getParentElement,
+  invokeQuery,
+  normalizeKeyframes,
+  validateStyleProperty,
+  validateWebAnimatableStyleProperty
+} from "./chunk-NQXDZHHT.js";
+import "./chunk-C5WYEU4P.js";
+import "./chunk-KWHJKGWW.js";
+import "./chunk-452SNCNP.js";
+import "./chunk-WSA2QMXP.js";
+import "./chunk-EGKP3FWG.js";
+export {
+  AnimationDriver,
+  NoopAnimationDriver,
+  Animation as ɵAnimation,
+  AnimationEngine as ɵAnimationEngine,
+  AnimationRenderer as ɵAnimationRenderer,
+  AnimationRendererFactory as ɵAnimationRendererFactory,
+  AnimationStyleNormalizer as ɵAnimationStyleNormalizer,
+  BaseAnimationRenderer as ɵBaseAnimationRenderer,
+  NoopAnimationStyleNormalizer as ɵNoopAnimationStyleNormalizer,
+  WebAnimationsDriver as ɵWebAnimationsDriver,
+  WebAnimationsPlayer as ɵWebAnimationsPlayer,
+  WebAnimationsStyleNormalizer as ɵWebAnimationsStyleNormalizer,
+  allowPreviousPlayerStylesMerge as ɵallowPreviousPlayerStylesMerge,
+  camelCaseToDashCase as ɵcamelCaseToDashCase,
+  containsElement as ɵcontainsElement,
+  createEngine as ɵcreateEngine,
+  getParentElement as ɵgetParentElement,
+  invokeQuery as ɵinvokeQuery,
+  normalizeKeyframes as ɵnormalizeKeyframes,
+  validateStyleProperty as ɵvalidateStyleProperty,
+  validateWebAnimatableStyleProperty as ɵvalidateWebAnimatableStyleProperty
+};
+//# sourceMappingURL=browser-6SEB2AEU.js.map
