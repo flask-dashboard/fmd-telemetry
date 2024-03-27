a243b2f8b2aa8152e966d4ccbbae5489b1f9aebf
johc <johc@itu.dk>
2024-03-27 13:38:29 +0100

    Updates to fetch script

--- fetch_data.py
+++ fetch_data.py
@@ -6,20 +6,15 @@ import os
 client = MongoClient(os.environ['MONGODB_URI'])
 db = client.FMD  # Assuming 'FMD' is your database name
 
-# Fetch data from FMD.Endpoints collection
-endpoints_data = list(db.Endpoints.find({}))
+# Function to fetch and save collection data
+def fetch_and_save(collection_name, file_path):
+    data = list(db[collection_name].find({}))
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, 'w') as file:
+        json.dump(data, file)
 
-# Save Endpoints data to a JSON file
-endpoints_file_path = 'fmd-telemetry/fmd-telemetry/src/assets/Endpoints.json'
-os.makedirs(os.path.dirname(endpoints_file_path), exist_ok=True)
-with open(endpoints_file_path, 'w') as file:
-    json.dump(endpoints_data, file)
+# Fetch and save FMD.Endpoints
+fetch_and_save("Endpoints", 'fmd-telemetry/fmd-telemetry/src/assets/Endpoints.json')
 
-# Fetch data from FMD.UserSession collection
-user_session_data = list(db.UserSession.find({}))
-
-# Save UserSession data to a JSON file
-user_session_file_path = 'fmd-telemetry/fmd-telemetry/src/assets/UserSession.json'
-os.makedirs(os.path.dirname(user_session_file_path), exist_ok=True)
-with open(user_session_file_path, 'w') as file:
-    json.dump(user_session_data, file)
+# Fetch and save FMD.UserSession
+fetch_and_save("UserSession", 'fmd-telemetry/fmd-telemetry/src/assets/UserSession.json')
