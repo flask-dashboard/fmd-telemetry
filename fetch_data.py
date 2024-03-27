275a6cbb21e7b9fb1483594e22f5dc9512c01f89
johc <johc@itu.dk>
2024-03-27 13:56:19 +0100

    Added json encode

--- fetch_data.py
+++ fetch_data.py
@@ -1,17 +1,25 @@
 from pymongo import MongoClient
 import json
 import os
+from bson import ObjectId
+
+# JSON Encoder for handling MongoDB ObjectId
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
 
 # Connect to MongoDB
 client = MongoClient(os.environ['MONGODB_URI'])
-db = client.FMD  # Assuming 'FMD' is your database name
+db = client.FMD  # Replace 'FMD' with your actual database name
 
 # Function to fetch and save collection data
 def fetch_and_save(collection_name, file_path):
     data = list(db[collection_name].find({}))
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     with open(file_path, 'w') as file:
-        json.dump(data, file)
+        file.write(JSONEncoder().encode(data))
 
 # Fetch and save FMD.Endpoints
 fetch_and_save("Endpoints", 'fmd-telemetry/fmd-telemetry/src/assets/Endpoints.json')
